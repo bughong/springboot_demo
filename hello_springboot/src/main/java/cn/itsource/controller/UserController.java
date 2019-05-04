2f1abad1d1a799c0df949e258287187eeda247d3
htw <283856186@qq.com>
2019-05-04 19:49:22 +0800

    提交

--- hello_springboot/src/main/java/cn/itsource/controller/UserController.java
+++ hello_springboot/src/main/java/cn/itsource/controller/UserController.java
@@ -1,12 +1,14 @@
 package cn.itsource.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class UserController {
     @RequestMapping(value = "/login")
     public String login(String name){
-        return name+"1234";
+        return "index";
     }
 }
